e929f0cee174511270034eb44bfc4bcb80436a02
Mike Bloom <mike.bloom93@gmail.com>
2015-10-12 18:31:44 -0400

    recursive update

--- MSIndeed/MSIndeed/spiders/Indeed.py
+++ MSIndeed/MSIndeed/spiders/Indeed.py
@@ -23,12 +23,9 @@ class myspider(CrawlSpider):
     name = "myspider"
     allowed_domains = ["indeed.com","brassring.com"]
     start_urls = ('http://www.indeed.com/jobs?q=PSEG+OR+%22Con+Edison%22&l=Huntington+Station%2C+NY',)
-    #rules=(Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="resultsCol"]/div/a/span/span',)), callback="parse_page", follow= True),)
+    #rules=(Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@class="pagination"]',)), callback="parse_page", follow= True),)
 
-    def start_requests(self):
-        for i in xrange(1000):
-            yield self.make_requests_from_url('http://www.indeed.com/jobs?q=PSEG+OR+%22Con+Edison%22&l=Huntington+Station%2C+NY&start=%d' %i)
-            
+
     def parse(self, response):
         print "****STEP 1****"
         jobs = scrapy.Selector(response).xpath('//*[@id="resultsCol"]/div/h2')
@@ -44,17 +41,28 @@ class myspider(CrawlSpider):
         for link in urls:
             yield Request(urljoin(response.url, link[1:]),callback=self.parse_url)
             
+        #for link in myLinks:
+            #tempURL = self.make_requests_from_url(link)
+                #yield Request(url=link,callback=self.parse_data(response))
+            #yield self.make_requests_from_url(link)
     def parse_url(self,response):
-        print "******RESPONSE URL******"
-        jobs = scrapy.Selector(response).xpath('//table')
         
-        myJob = MsindeedItem()
-        for job in jobs:
-            myJob['positionTitle'] = jobs.xpath('//*[@id="Title"]/text()').extract()
-            myJob['positionDescription'] = jobs.xpath('//*[@id="Job Description"]/text()').extract()
-            myJob['positionLocation'] = jobs.xpath('//*[@id="Location"]/text()').extract()
-            yield myJob
-
-        pass
+        #print "******RESPONSE URL******"
+        
+        #XPath parameters for brassring HR site
+        if("brassring" in response.url):
+            jobs = scrapy.Selector(response).xpath('//table')
+            title = jobs.xpath('//*[@id="Title"]/text()').extract()
+            description = jobs.xpath('//*[@id="Job Description"]/text()').extract()
+            location = jobs.xpath('//*[@id="Location"]/text()').extract()
+            print title, description,location
+            
+        if("indeed.com" in response.url):
+            print "test"
+            jobs = scrapy.Selector(response).xpath('//table/tbody/tr/td')
+            title = jobs.xpath('//*[@id="job_summary"]/p[1]/b[2]/text()').extract()
+            description = jobs.xpath('//*[@id="job_summary"]/ul[1]/text()').extract()
+            location = jobs.xpath('//*[@id="job_summary"]/p[1]/b[4]/text()').extract()
+        #pass
         
     
